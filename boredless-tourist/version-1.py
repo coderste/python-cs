6ef51039c79eaab555623b6e6f04bacfd82c8669
Stephen Hinett <stephen.hinett@skybettingandgaming.com>
2019-05-20 17:08:00 +0100

    Started the logic around traveler destinations and convert to internal data

--- boredless-tourist/version-1.py
+++ boredless-tourist/version-1.py
@@ -2,3 +2,26 @@ destinations = ['Paris, France', 'Shanghai, China',
                 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+attractions = [[] for i in destinations]
+
+
+def get_dest_index(destination):
+    return destinations.index(destination)
+
+
+def get_traveler_location(traveler):
+    traveler_dest = traveler[1]
+    dest_index = get_dest_index(traveler_dest)
+    return dest_index
+
+
+def add_attraction(destination, attraction):
+    try:
+        dest_index = get_dest_index(destination)
+    except SyntaxError:
+        print("Error caught!")
+    attraction.
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
