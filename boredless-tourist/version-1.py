d90e2d96d84f8b74265e1b020aab7b079476df84
Stephen <me@stephenhinett.co.uk>
2019-05-20 20:47:11 +0100

    Added .gitignore and created attractions and functionality for adding new attractions

--- boredless-tourist/version-1.py
+++ boredless-tourist/version-1.py
@@ -19,9 +19,27 @@ def get_traveler_location(traveler):
 def add_attraction(destination, attraction):
     try:
         dest_index = get_dest_index(destination)
+        attractions_for_destination = attractions[dest_index]
+        attractions_for_destination.append(attraction)
     except SyntaxError:
-        print("Error caught!")
-    attraction.
+        return
+
 
 test_destination_index = get_traveler_location(test_traveler)
 print(test_destination_index)
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", [
+               "historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", [
+               "garden", "historcical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", [
+               "skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", [
+               "monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+print(attractions)
